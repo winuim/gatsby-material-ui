255560acaabfb82c4e6dd53d918b03d1bb8d7476
Uema Yohei <winuim@gmail.com>
2020-03-17 22:56:28 +0900

    typescript化

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import MuiLink from '@material-ui/core/Link';
-import ProTip from '../components/ProTip';
-import Link from '../components/Link';
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import MuiLink from "@material-ui/core/Link";
+import ProTip from "../components/ProTip";
+import { Link } from "gatsby";
 
-function Copyright() {
+function Copyright(): JSX.Element {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <MuiLink color="inherit" href="https://material-ui.com/">
         Your Website
-      </MuiLink>{' '}
+      </MuiLink>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
-export default function About() {
+export default function About(): JSX.Element {
   return (
     <Container maxWidth="sm">
       <Box my={4}>
