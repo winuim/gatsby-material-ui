8e67ca967b36096a20f1f05eb6a74f15aeaf4d63
Uema Yohei <winuim@gmail.com>
2020-03-18 11:23:47 +0900

    firebase plugin1削除１ テーマをデフォルトに変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
- const activeEnv =
+const activeEnv =
   process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
 console.log(`Using environment config: '${activeEnv}'`)
 
@@ -52,23 +52,6 @@ module.exports = {
       resolve: 'gatsby-plugin-graphql-codegen',
       options: {
         fileName: 'types/graphql-types.d.ts',
-      },
-    },
-    {
-      resolve: "gatsby-theme-firebase",
-      options: {
-        credentials: {
-          apiKey: process.env.FIREBASE_API_KEY,
-          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-          databaseURL: process.env.FIREBASE_DATABASE_URL,
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-          appId: process.env.FIREBASE_APP_ID,
-        },
-        loginPath: "/login",
-        loginRedirectPath: "/dashboard",
-        socialLogins: ["google", "twitter", "facebook", "github"],
       },
     },
     "gatsby-plugin-top-layout",
